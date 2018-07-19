933a96adcb012adb269312f5d8f22278f9058669
lizekun <soap@easyspark.cn>
2018-07-19 13:26:18 +0800

    init

---
+++ index.js
@@ -0,0 +1,18 @@
+import App from './App'
+import {Navigation} from 'react-native-navigation'
+
+Navigation.registerComponent('App',()=>App)
+
+Navigation.events().registerAppLaunchedListener(() => {
+    Navigation.setRoot({
+        root: {
+            stack: {
+                children: [{
+                    component: {
+                        name: 'App'
+                    }
+                }]
+            }
+        }
+    });
+});
